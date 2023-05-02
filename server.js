a8fa50725cd5dcab7937675ac877f96f42cec918
Biz <github@bisrat.dev>
2023-05-02 14:29:13 -0600

    fix static bucket

--- server.js
+++ server.js
@@ -15,12 +15,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // handle get for webpage traversing
-// app.get('/', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/index.html'));
-// })
-// app.get('/notes', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'public/notes.html'));
-// })
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/index.html'));
+})
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'));
+})
 
 
 // handle get for api
